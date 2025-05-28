d907a6a2a04a54a94150bbed36a6a50bf04d86ba
Stefan374245 <shelldobler@gmx.de>
2025-05-28 13:58:13 +0200

    out.html  of errorHandling, script, userLink , userVerification.js

--- scripts/userVerification.js
+++ scripts/userVerification.js
@@ -1,3 +1,7 @@
+/**
+ * Checks if a user is stored in localStorage under the key "name".
+ * If no user is found, redirects the browser to the index page.
+ */
 function checkLocalstorage() {
     const user = localStorage.getItem("name");
     
